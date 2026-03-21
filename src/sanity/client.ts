cca31b809dd9e47e38f2443b2a8501d7e7b6fead
DogukanBayraktar <dogukan.bayraktar11@gmail.com>
2026-03-21 22:59:14 +0300

    client-degisiklik

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -1,6 +1,5 @@
 import { createClient } from 'next-sanity';
 import imageUrlBuilder from '@sanity/image-url';
-import type { SanityImageSource } from '@sanity/image-url/lib/types/types';
 
 export const client = createClient({
   projectId: 'sg46eh28',
@@ -10,6 +9,6 @@ export const client = createClient({
 });
 
 const builder = imageUrlBuilder(client);
-export function urlFor(source: SanityImageSource) {
+export function urlFor(source: any) {
   return builder.image(source);
 }
