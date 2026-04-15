d76115f9ad50123d22f496c192dfda5c3ec3bfd9
DogukanBayraktar <dogukan.bayraktar11@gmail.com>
2026-04-15 21:41:45 +0300

    güncelleme

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,1 +1,8 @@
-export { default, revalidate } from '../galeri/page';
+import GaleriPageClient from '@/components/galeri/GaleriPageClient';
+import { GALLERY_CATEGORIES } from '@/lib/gallery';
+
+export const revalidate = 3600;
+
+export default function GalleryPage() {
+  return <GaleriPageClient initialCategories={GALLERY_CATEGORIES} />;
+}
