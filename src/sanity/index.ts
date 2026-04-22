51502253986aa3152014e893b12b325aae50545b
DogukanBayraktar <dogukan.bayraktar11@gmail.com>
2026-04-22 23:44:34 +0300

    remove sanity

--- src/sanity/index.ts
+++
@@ -1,5 +0,0 @@
-import { blogSchema } from './schemas/blog';
-import { treatmentSchema } from './schemas/treatment';
-import { testimonialSchema } from './schemas/testimonial';
-
-export const schemaTypes = [blogSchema, treatmentSchema, testimonialSchema];
