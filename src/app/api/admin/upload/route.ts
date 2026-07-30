d1d782c4492e4585b13c6e624d000ed49d683130
dogukan bayraktar <dogukan.bayraktar11@gmail.com>
2026-07-30 11:47:17 +0300

    sorun-v1

--- src/app/api/admin/upload/route.ts
+++ src/app/api/admin/upload/route.ts
@@ -83,6 +83,7 @@ export async function POST(request: Request): Promise<NextResponse> {
       access: 'public',
       token: process.env.BLOB_READ_WRITE_TOKEN,
       contentType: detectedMime,
+      addRandomSuffix: true,
     });
 
     return NextResponse.json(blob);
